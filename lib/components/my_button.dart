b0623127d1e80f80bb096a72adf5d61b0b9e4c6a
DerrickLegacy <ahaabwederrick67@gmail.com>
2024-10-10 15:19:53 +0300

    chore: registering using firebaseAuth with a pre-created user

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
   final Function()? onTap;
-
   const MyButton({
     super.key,
     required this.onTap,
@@ -13,23 +12,18 @@ class MyButton extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        padding: const EdgeInsets.all(25.0),
-        margin: const EdgeInsets.symmetric(horizontal: 25.0),
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: const Center(
-          child: Text(
-            "Sign In",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
-            ),
+          padding: const EdgeInsets.all(25.0),
+          margin: const EdgeInsets.symmetric(horizontal: 25.0),
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(8),
           ),
-        ),
-      ),
+          child: const Center(
+              child: Text(
+            'Sign In',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
+          ))),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AuthPage(),
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+        color: Colors.amber[100],
+      )),
+      home: const AuthPage(),
     );
   }
 }
